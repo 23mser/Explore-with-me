e7d081b98fb5f9138882d877e147de696f72ccfa
Сергей <moskovets2319@yandex.ru>
2024-01-22 11:52:26 +0300

    исключение NullPointerException

--- ewm-main/src/main/java/ru/practicum/ewm/categories/dto/CategoryMapper.java
+++ ewm-main/src/main/java/ru/practicum/ewm/categories/dto/CategoryMapper.java
@@ -2,6 +2,7 @@ package ru.practicum.ewm.categories.dto;
 
 import lombok.experimental.UtilityClass;
 import ru.practicum.ewm.categories.model.Category;
+import ru.practicum.ewm.exceptions.IncorrectRequestException;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -10,12 +11,18 @@ import java.util.stream.Collectors;
 public class CategoryMapper {
 
     public Category toCategory(CategoryDto categoryDto) {
+        if (categoryDto.getName() == null) {
+            throw new IncorrectRequestException("Имя не может быть пустым.");
+        }
         return Category.builder()
                 .name(categoryDto.getName())
                 .build();
     }
 
     public CategoryDto toCategoryDto(Category category) {
+        if (category.getName() == null || category.getId() == null) {
+            throw new IncorrectRequestException("Имя/id не может быть пустым.");
+        }
         return CategoryDto.builder()
                 .id(category.getId())
                 .name(category.getName())
--- ewm-main/src/main/java/ru/practicum/ewm/compilatons/dto/CompilationMapper.java
+++ ewm-main/src/main/java/ru/practicum/ewm/compilatons/dto/CompilationMapper.java
@@ -3,6 +3,7 @@ package ru.practicum.ewm.compilatons.dto;
 import lombok.experimental.UtilityClass;
 import ru.practicum.ewm.compilatons.model.Compilation;
 import ru.practicum.ewm.events.dto.EventMapper;
+import ru.practicum.ewm.exceptions.IncorrectRequestException;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -10,6 +11,9 @@ import java.util.stream.Collectors;
 @UtilityClass
 public class CompilationMapper {
     public CompilationDto toCompilationDto(Compilation compilation) {
+        if (compilation.getTitle().isEmpty()) {
+            throw new IncorrectRequestException("Заголовок подборки не может быть пустым.");
+        }
         return CompilationDto.builder()
                 .id(compilation.getId())
                 .events(EventMapper.toEventShortDtoList(compilation.getEvents()))
@@ -19,6 +23,9 @@ public class CompilationMapper {
     }
 
     public Compilation toCompilation(NewCompilationDto dto) {
+        if (dto.getTitle().isEmpty()) {
+            throw new IncorrectRequestException("Заголовок подборки не может быть пустым.");
+        }
         return Compilation.builder()
                 .pinned(dto.getPinned())
                 .title(dto.getTitle())
